ff6e8a484a2cdf99cdfb30d1432825b29ece2920
kfmahre <kfmahre@gmail.com>
2016-07-16 23:05:08 -0400

    changed some html within index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ main_page_head = '''
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Fresh Tomatoes!</title>
+    <title>Kyle's Movie Trailer Website</title>
 
     <!-- Bootstrap 3 -->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
@@ -107,7 +107,7 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+            <a class="navbar-brand" href="#">Kyle's Movie Trailers</a>
           </div>
         </div>
       </div>
